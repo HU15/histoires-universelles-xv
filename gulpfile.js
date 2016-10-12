ad0974820fd2e6f35bbb1998ce3fab092f853b44
Ouzned <ouzned.alleykatze@gmail.com>
2016-10-12 20:52:12 +0200

    Modification des pages xhtml de la racine du site

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@ var fileinclude = require('gulp-file-include');
 var clean = require('gulp-clean');
 var gulp = require('gulp');
 
-gulp.task('clean-pages', function () {
+gulp.task('clean-docs', function () {
     return gulp.src('./docs', {read: false})
       .pipe(clean());
 });
 
-gulp.task('copy-resources', ['clean-pages'], function() {
+gulp.task('copy-resources', ['clean-docs'], function() {
     gulp.src(['./style/**' ])
       .pipe(gulp.dest('./docs/style'));
 });
 
 gulp.task('default', ['copy-resources'], function() {
-    gulp.src(['./**/*.xhtml', '!./fragments/**', '!./node_modules/**' ])
+    gulp.src(['./**/*.xhtml', '!./fragments/**', '!./node_modules/**', '!temp.xhtml', '!templet-general.xhtml' ])
       .pipe(fileinclude({
         prefix: '@@',
         basepath: '@file'
